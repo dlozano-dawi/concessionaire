118a6c6d4848c961815030e0a692cc138912f3f1
examengit <gitexamen@gmail.com>
2024-04-16 13:18:43 +0200

    test

--- lists/types/CarLists.java
+++ lists/types/CarLists.java
@@ -10,9 +10,9 @@ import vehicles.types.Cars;
 public class CarLists implements VehiclesLists{
     List<Cars> carList = new ArrayList<Cars>();
 
-    public CarLists(Cars car) {
-        this.carList.add(car);
-    }
+    // public CarLists(Cars car) {
+    //     this.carList.add(car);
+    // }
 
     public void setCarlist(Cars car) {
         this.carList.add(car);
@@ -22,19 +22,27 @@ public class CarLists implements VehiclesLists{
         List<Cars> carListCopy = carList;
         String response = "";
 
+        if (carListCopy.size() == 0) {
+            return "0 Founds";
+        }
+        
         Collections.sort(carListCopy, (o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()));
 
         for (int i = 0; i < carListCopy.size(); i++) {
             response += carListCopy.get(i).getBrand() + " " + carListCopy.get(i).getModel() + " " + carListCopy.get(i).getPrice() + "\n";
         }
 
-        return response;
+        return "Founds:\n" + response;
     } 
 
     public String orderDesc() {
         List<Cars> carListCopy = carList;
         String response = "";
 
+        if (carListCopy.size() == 0) {
+            return "0 Founds";
+        }
+        
         Collections.sort(carListCopy, (o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()));
         Collections.reverse(carListCopy);
 
@@ -42,7 +50,7 @@ public class CarLists implements VehiclesLists{
             response += carListCopy.get(i).getBrand() + " " + carListCopy.get(i).getModel() + " " + carListCopy.get(i).getPrice() + "\n";
         }
 
-        return response;
+        return "Founds:\n" + response;
     } 
 
     public String limitMin(double min) {
@@ -55,6 +63,10 @@ public class CarLists implements VehiclesLists{
             }
         }
 
+        if (carListCopy.size() == 0) {
+            return "0 Founds";
+        }
+
         Collections.sort(carListCopy, (o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()));
         Collections.reverse(carListCopy);
 
@@ -62,7 +74,7 @@ public class CarLists implements VehiclesLists{
             response += carListCopy.get(i).getBrand() + " " + carListCopy.get(i).getModel() + " " + carListCopy.get(i).getPrice() + "\n";
         }
 
-        return response;
+        return "Founds:\n" + response;
     } 
     public String limitMax(double max) {
         List<Cars> carListCopy = new ArrayList<Cars>();
@@ -74,6 +86,10 @@ public class CarLists implements VehiclesLists{
             }
         }
 
+        if (carListCopy.size() == 0) {
+            return "0 Founds";
+        }
+
         Collections.sort(carListCopy, (o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()));
         Collections.reverse(carListCopy);
 
@@ -81,6 +97,29 @@ public class CarLists implements VehiclesLists{
             response += carListCopy.get(i).getBrand() + " " + carListCopy.get(i).getModel() + " " + carListCopy.get(i).getPrice() + "\n";
         }
 
-        return response;
-    } 
+        return "Founds:\n" + response;
+    }
+    public String limitMinAndMax(double min, double max) {
+        List<Cars> carListCopy = new ArrayList<Cars>();
+        String response = "";
+
+        for (int i = 0; i < carList.size(); i++) {
+            if (carList.get(i).getPrice() >= min && carList.get(i).getPrice() <= max) {
+                carListCopy.add(carList.get(i));
+            }
+        }
+
+        if (carListCopy.size() == 0) {
+            return "0 Founds";
+        }
+
+        Collections.sort(carListCopy, (o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()));
+        Collections.reverse(carListCopy);
+
+        for (int i = 0; i < carListCopy.size(); i++) {
+            response += carListCopy.get(i).getBrand() + " " + carListCopy.get(i).getModel() + " " + carListCopy.get(i).getPrice() + "\n";
+        }
+
+        return "Founds:\n" + response;
+    }
 }
--- main/Main_Activity.java
+++ main/Main_Activity.java
@@ -1,6 +1,7 @@
 package main;
 
 import lists.types.CarLists;
+import lists.types.F1Lists;
 import vehicles.types.*;
 public class Main_Activity {
 
@@ -15,20 +16,26 @@ public class Main_Activity {
         // System.out.println(nissanSilvia.getBrand());
         // System.out.println(nissanSilvia.getModel());
         // System.out.println(nissanSilvia.getPrice());
-        Cars nissanSilvia1 = new Cars(2, "coupe", 100000, 270, false, "Nissan1", "Silvia 240sx", 20000);
-        Cars nissanSilvia2 = new Cars(2, "coupe", 100000, 270, false, "Nissan2", "Silvia 240sx", 21000);
-        Cars nissanSilvia3 = new Cars(2, "coupe", 100000, 270, false, "Nissan3", "Silvia 240sx", 10000);
-        Cars nissanSilvia4 = new Cars(2, "coupe", 100000, 270, false, "Nissan4", "Silvia 240sx", 25000);
 
-        CarLists listtt = new CarLists(nissanSilvia1);
-        listtt.setCarlist(nissanSilvia2);
-        listtt.setCarlist(nissanSilvia3);
-        listtt.setCarlist(nissanSilvia4);
+        // Cars nissanSilvia1 = new Cars(2, "coupe", 100000, 270, false, "Nissan1", "Silvia 240sx", 20000);
+        // Cars nissanSilvia2 = new Cars(2, "coupe", 100000, 270, false, "Nissan2", "Silvia 240sx", 21000);
+        // Cars nissanSilvia3 = new Cars(2, "coupe", 100000, 270, false, "Nissan3", "Silvia 240sx", 10000);
+        // Cars nissanSilvia4 = new Cars(2, "coupe", 100000, 270, false, "Nissan4", "Silvia 240sx", 25000);
 
-        System.out.println(listtt.orderAsc());
-        System.out.println(listtt.orderDesc());
-        System.out.println(listtt.limitMin(11000));
-        System.out.println(listtt.limitMax(20000));
+        CarLists listOfCars = new CarLists();
+        F1Lists listOfF1 = new F1Lists();
+        Motorbikes listOfMotorbikes = new Motorbikes();
+        Motorcicle listOfMotorcicle = new Motorcicle();
 
+        listOfCars.setCarlist(new Cars(2, "coupe", 100000, 200, false, "Nissan", "350z", 20000));
+        listOfCars.setCarlist(new Cars(2, "coupe", 100000, 300, false, "Nissan", "370z", 21000));
+        listOfCars.setCarlist(new Cars(2, "coupe", 100000, 120, false, "Toyota", "Celica", 10000));
+        listOfCars.setCarlist(new Cars(2, "coupe", 100000, 270, false, "Nissan", "Silvia 240sx", 25000));
+
+        System.out.println(listOfCars.orderAsc());
+        System.out.println(listOfCars.orderDesc());
+        System.out.println(listOfCars.limitMin(11000));
+        System.out.println(listOfCars.limitMax(20000));
+        System.out.println(listOfCars.limitMinAndMax(12000, 20000));
     }
 }
